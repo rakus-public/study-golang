e2ec9ade5e7d52cfcd509782ed62f9e1a1ef1e25
pey-chan <yohei.uemura@rakus.co.jp>
2023-03-27 10:10:25 +0900

    make tutorial blank

--- tutorial/subject/chapter09/fare.go
+++ tutorial/subject/chapter09/fare.go
@@ -1,103 +1,31 @@
 package chapter09
 
-import "tutorial/helper"
-
 type Fare struct {
 	From string
 	To   string
 }
 
 func (f *Fare) innerDistance() int {
-	if f.From == f.To {
-		return 0
-	}
-
-	var dist int
-	current := f.From
-	for i := 0; i < 30; i++ {
-		current = helper.InnerNextStation(current)
-		dist += helper.InnerLoopDistance(current)
-		if current == f.To {
-			break
-		}
-	}
-
-	return dist
+	// TODO: 内回りの距離を返すように実装
+	return 0
 }
 
 func (f *Fare) outerDistance() int {
-	if f.From == f.To {
-		return 0
-	}
-
-	var dist int
-	current := f.From
-	for i := 0; i < 30; i++ {
-		current = helper.OuterNextStation(current)
-		dist += helper.OuterLoopDistance(current)
-		if current == f.To {
-			break
-		}
-	}
-
-	return dist
+	// TODO: 外回りの距離を返すように実装
+	return 0
 }
 
 func (f *Fare) distance() int {
-	id := f.innerDistance()
-	od := f.outerDistance()
-
-	if id > od {
-		return od
-	}
-
-	return id
+	// 内回りと外回りの距離を比較し、短い方を返すように実装
+	return 0
 }
 
 func (f *Fare) TicketCharge() int {
-	dist := f.distance()
-	switch {
-	case dist == 0:
-		return 0
-	case dist < 4000:
-		return 140
-	case dist < 7000:
-		return 160
-	case dist < 11000:
-		return 170
-	case dist < 16000:
-		return 200
-	case dist < 21000:
-		return 270
-	case dist < 26000:
-		return 350
-	case dist < 31000:
-		return 420
-	default:
-		return 490
-	}
+	// TODO: 切符の料金を返すように実装
+	return 0
 }
 
 func (f *Fare) CardCharge() int {
-	dist := f.distance()
-	switch {
-	case dist == 0:
-		return 0
-	case dist < 4000:
-		return 136
-	case dist < 7000:
-		return 157
-	case dist < 11000:
-		return 168
-	case dist < 16000:
-		return 198
-	case dist < 21000:
-		return 264
-	case dist < 26000:
-		return 341
-	case dist < 31000:
-		return 418
-	default:
-		return 484
-	}
+	// TODO: IC カードの料金を返すように実装
+	return 0
 }
--- tutorial/subject/chapter09/ticket.go
+++ tutorial/subject/chapter09/ticket.go
@@ -5,35 +5,14 @@ type Charger interface {
 	Use(charge int)
 }
 
+// Charger を満たすようにメソッドを追加する
 type Ticket struct {
 	Price int
 	Used  bool
 }
 
-func (t *Ticket) Amount() int {
-	return t.Price
-}
-
-func (t *Ticket) Use(int) {
-	t.Used = true
-}
-
+// Charger を満たすようにメソッドを追加する
 type Card struct {
 	Balance int
 	Point   int
-}
-
-func (c *Card) Amount() int {
-	return c.Balance + c.Point
-}
-
-func (c *Card) Use(charge int) {
-	if c.Point > charge {
-		c.Point -= charge
-		return
-	}
-
-	remain := charge - c.Point
-	c.Balance -= remain
-	c.Point = 0
 }
